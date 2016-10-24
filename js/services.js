7cb7e9a4b6e649c4a4bd93e79e86a531b61d25e6
Gina <gdebell@gmail.com>
2016-10-24 13:28:00 -0600

    update

--- js/services.js
+++ js/services.js
@@ -7,13 +7,13 @@
     .service('coffeeService', coffeeService
   );
 
-  coffeeService.$inject = ['http'];
+  coffeeService.$inject = ['$http'];
 
   function coffeeService($http) {
     /*jshint validthis: true */
     this.test = 'does this work?';
     this.getAllCoffee = function () {
-      return $http.get('https://stormy-atoll-55298.herokuapp.com/coffee/1');
+      return $http.get('//stormy-atoll-55298.herokuapp.com/coffee/1');
     };
     this.test = 'does this shit work?';
   }
