3d4c887350db7a51944125b47a6b8cd9b9e2f201
Gina <gdebell@gmail.com>
2016-10-24 13:21:42 -0600

    connect with server

---
+++ js/services.js
@@ -0,0 +1,21 @@
+(function() {
+
+  'use strict';
+
+  angular
+    .module('myApp.services', [])
+    .service('coffeeService', coffeeService
+  );
+
+  coffeeService.$inject = ['http'];
+
+  function coffeeService($http) {
+    /*jshint validthis: true */
+    this.test = 'does this work?';
+    this.getAllCoffee = function () {
+      return $http.get('https://stormy-atoll-55298.herokuapp.com/coffee/1');
+    };
+    this.test = 'does this shit work?';
+  }
+
+})();
